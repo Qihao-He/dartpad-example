c4b34c6245357f77df7fb89c67fd0844230c7b85
Qihao He <qi.he@maine.edu>
2019-07-29 11:18:58 -0700

    valid email

---
+++ word_guessing/word_gussing.dart
@@ -0,0 +1,15 @@
+import 'dart:html';
+
+void main() {
+  final ButtonElement button = querySelector('button');
+  final InputElement input = querySelector('input');
+  final String secretWord = 'banana';
+  
+  button.onClick
+    .take(4)
+    .where((event) => input.value == secretWord)
+    .listen(
+      (event) => print('You got it!'),
+      onDone: () => print('Nope, bad guesses.')
+    );
+}
