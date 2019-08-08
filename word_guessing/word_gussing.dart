93abc0d2a9f431a7028f1e1ccb2587dba04cbb50
Qihao He <qi.he@maine.edu>
2019-08-07 23:54:13 -0700

    Streams take and where functions

--- word_guessing/word_gussing.dart
+++ word_guessing/word_gussing.dart
@@ -5,12 +5,7 @@ void main() {
   final InputElement input = querySelector('input');
   final String secretWord = 'banana';
 
-  button.onClick
-      .take(4)
-      .where(
-        (event) => input.value == secretWord,
-      )
-      .listen(
+  button.onClick.take(4).where((event) => input.value == secretWord).listen(
         (event) => print('You got it!'),
         onDone: () => print('Nope, bad guesses.'),
       );
