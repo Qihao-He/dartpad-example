7dd18cb7df47fe8cd355705f2d029a3abeda2c9a
Qihao He <qi.he@maine.edu>
2019-08-08 11:09:13 -0700

    Wiring up the Error

--- valid_email/valid_email.dart
+++ valid_email/valid_email.dart
@@ -4,18 +4,22 @@ import 'dart:async';
 void main() {
   final InputElement input = querySelector('input');
   final DivElement div = querySelector('div');
-  
-  final validator = new StreamTransformer.fromHandlers(
+
+  final validatorEmail = new StreamTransformer.fromHandlers(
     handleData: (inputValue, sink) {
-      if(inputValue.contains('@')) {
+      if (inputValue.contains('@')) {
         sink.add(inputValue);
       } else {
         sink.addError('Enter a valid email');
-     }
+      }
     },
   );
 
   input.onInput
-    .map((dynamic event) => event.target.value)
-    .transform(validator)
+      .map((dynamic event) => event.target.value)
+      .transform(validatorEmail)
+      .listen(
+        (inputValue) => div.innerHtml = '',
+        onError: (err) => div.innerHtml = err,
+      );
 }
