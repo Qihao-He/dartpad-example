99be054d51e4581e7789e848f96b022389457082
Qihao He <qi.he@maine.edu>
2019-08-13 14:54:40 -0700

    add snackBar and alert dialog for display message

--- push_notification/lib/main.dart
+++ push_notification/lib/main.dart
@@ -1,61 +1,18 @@
-import 'package:flutter/material.dart';
-
 import 'dart:async';
 import 'dart:io';
 
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import './src/message_handler.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  @overrider
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: MessageHandler(),
       ),
     );
-  }
-}
-
-class Messagehandler extends StatefulWidget {
-  @override
-  createState() => _MessageHandlerState();
-}
-
-class _MessaageHandlerState extends State<MessageHandler> {
-  final FireStore _db = Firestore.instance;
-  final FirebaseMessaging _fcm = FirebaseMessaging();
-
-  @override
-  void initState() {
-    super.initState();
-    _fcm.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-
-        final snackbar = SnackBar(
-          context: Text(message['notification']['title']),
-          action: SnackBarAction(
-            label: 'Go',
-            onPressed: () => null,
-          ),
-        );
-
-        Scaffold.of(context).showSnackBar();
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-      },
-    );
-  }
-
-  @overrider
-  Widget build(BuildContext context) {
-    return null;
   }
 }
---
+++ push_notification/lib/src/message_handler.dart
@@ -0,0 +1,59 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class MessageHandler extends StatefulWidget {
+  @override
+  createState() => _MessageHandlerState();
+}
+
+class _MessageHandlerState extends State<MessageHandler> {
+  final Firestore _db = Firestore.instance;
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  @override
+  void initState() {
+    super.initState();
+    _fcm.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+
+        final snackbar = SnackBar(
+          content: Text(message['notification']['title']),
+          action: SnackBarAction(
+            label: 'Go',
+            onPressed: () => null,
+          ),
+        );
+
+        Scaffold.of(context).showSnackBar(snackbar);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+        showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: ListTile(
+              title: Text(message['notification']['title']),
+              subtitle: Text(message['notification']['title']),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('Ok'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
